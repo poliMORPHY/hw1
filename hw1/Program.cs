a59bbdbc93301635c341f34e53c20c5dd21664c3
poliMORPHY <xexechan@inbox.ru>
2023-03-23 14:02:30 +0300

    Добавьте файлы проекта.

---
+++ hw1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //задание 1. Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+            //Console.WriteLine("Напишите первое число: ");
+            //int num1 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Напишите второе число: ");
+            //int num2 = Convert.ToInt32(Console.ReadLine());
+
+            //if (num1 < num2)
+            //    Console.WriteLine($"Наибольшее число {num2}, наименьшее число {num1}");
+            //else
+            //    Console.WriteLine($"Наибольшее число {num1}, наименьшее число {num2}");
+
+            //Console.ReadKey();
+
+            //задание 2. Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+            //Console.WriteLine("Напишите первое число: ");
+            //int num1 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Напишите второе число: ");
+            //int num2 = Convert.ToInt32(Console.ReadLine());
+            // Console.WriteLine("Напишите третье число: ");
+            //int num3 = Convert.ToInt32(Console.ReadLine());
+
+            //if (num1 > num2 & num1 > num3)
+            //    Console.WriteLine($"Наибольшее число {num1}");
+            //else if (num2 > num1 & num2 > num3)
+            //    Console.WriteLine($"Наибольшее число {num2}");
+            //else 
+            //    Console.WriteLine($"Наибольшее число {num3}");
+            // Console.ReadKey();
+
+            // задние 3. Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка). 
+            //Console.WriteLine("Напишите число, которое хотите проверить на четность: "); //оцените шутку на четность этого мира
+            //int num = Convert.ToInt32(Console.ReadLine());
+            //
+            //if (num % 2 == 0)
+            //    Console.WriteLine("Число четное");
+            //else
+            //    Console.WriteLine("Число нечетное");
+
+            //Console.ReadKey();
+
+            //задание 4. Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+            //Console.WriteLine("Напишите число: ");
+            //int num = Convert.ToInt32(Console.ReadLine());
+
+            //for (int c = 2; c <= num; c = c + 2)
+            //    Console.WriteLine(c);
+
+            //Console.ReadKey();
+        }
+    }
+}
